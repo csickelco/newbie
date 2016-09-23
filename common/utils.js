6a1dd152c9940b3a79eb2c4ea3d9ce8bf1a7a458
Christina Sickelco <csickelco@gmail.com>
2016-09-23 11:12:31 -0400

    Added util unit tests and fixed some bugs in utils

--- common/utils.js
+++ common/utils.js
@@ -21,9 +21,13 @@
 module.change_code = 1;
 var Utils = exports;
 
+//Constants
+var DAYS_PER_WEEK = 7; 
+var MS_IN_DAY = 24*60*60*1000;
+
 /**
  * Formats a text string describing weight in terms of pounds and ounces
- * @param	{number} a weight in ounces. Non-nullable.
+ * @param	{number} a weight in ounces. Non-nullable. Must be an integer > 0.
  * @returns a text string describing the given weight in terms of pounds and
  * 			ounces. E.g. if 195 is given, returns "12 pounds, 3 ounces"
  */
@@ -61,23 +65,84 @@ Utils.formatDateString = function(date){
 
 /**
  * Formats a string describing a baby's age given their birthdate
- * @param birthdate {Date} the baby's birthdate. Non-nullable.
+ * @param birthdate {Date} the baby's birthdate. Non-nullable. Must be < current date/time.
  * @returns a string describing a baby's age. e.g. "2 weeks and 5 days"
  */
 Utils.calculateAgeFromBirthdate = function(birthdate) {
 	var now = new Date();
-	var oneDay = 24*60*60*1000;
-	var daysPerWeek = 7; //TODO: Make constant
-	//TODO: report in months, days, or years if more appropriate
-	var diffWeeks = Math.floor(((now.getTime() - birthdate.getTime())/oneDay)/daysPerWeek);
-	var numDays = Math.floor((now.getTime() - birthdate.getTime())/oneDay % daysPerWeek);
-	var retval = "" + diffWeeks + " weeks";
-	if( numDays > 1 ) {
-		retval += " and " + numDays + " days";
-	} else if( numDays === 1 ) {
-		retval += " and " + numDays + " day";
+	
+	var ageInYears = Utils.calculateAge(birthdate);
+	var diffWeeks = Math.floor(((now.getTime() - birthdate.getTime())/MS_IN_DAY)/DAYS_PER_WEEK);
+	var numDays = Math.floor((now.getTime() - birthdate.getTime())/MS_IN_DAY % DAYS_PER_WEEK);
+	var retval = "";
+	
+	if( ageInYears > 0 ) {
+		retval += parseInt(ageInYears) + " year" + Utils.pluralizeIfNeeded(ageInYears);
+	}
+	else if( diffWeeks > 12 ) {
+		retval += parseInt(Utils.getMonthsBetween(birthdate, now, false)) + " months";
+	} else {
+		if( diffWeeks > 0 ) {
+			retval += diffWeeks + " week" + Utils.pluralizeIfNeeded(diffWeeks);
+			if( numDays > 0 ) {
+				retval += " and ";
+			}
+		} else if( numDays === 0 ) {
+			//Just born
+			retval += "0 days";
+		}
+			
+		if( numDays > 1 ) {
+			retval += numDays + " days";
+		} else if( numDays === 1 ) {
+			retval += numDays + " day";
+		}
 	}
 	return retval;
+};
+
+/**
+ * Returns the difference, in full months, between 2 dates
+ * See http://stackoverflow.com/questions/2536379/difference-in-months-between-two-dates-in-javascript/15158873#15158873
+ * @param date1 {Date} the first date to compare. Non-nullable. Must be < date2.
+ * @param date2 {Date} the second date to compare. Non-nullable. Must be > date1.
+ * @param roundUpFractionalMonths {boolean} true/false
+ * @returns {number} difference, in months, between d1 and d2
+ */
+Utils.getMonthsBetween = function(date1,date2,roundUpFractionalMonths)
+{
+    //Months will be calculated between start and end dates.
+    //Make sure start date is less than end date.
+    //But remember if the difference should be negative.
+    var startDate=date1;
+    var endDate=date2;
+    var inverse=false;
+    if(date1>date2)
+    {
+        startDate=date2;
+        endDate=date1;
+        inverse=true;
+    }
+
+    //Calculate the differences between the start and end dates
+    var yearsDifference=endDate.getFullYear()-startDate.getFullYear();
+    var monthsDifference=endDate.getMonth()-startDate.getMonth();
+    var daysDifference=endDate.getDate()-startDate.getDate();
+
+    var monthCorrection=0;
+    //If roundUpFractionalMonths is true, check if an extra month needs to be added from rounding up.
+    //The difference is done by ceiling (round up), e.g. 3 months and 1 day will be 4 months.
+    if(roundUpFractionalMonths===true && daysDifference>0)
+    {
+        monthCorrection=1;
+    }
+    //If the day difference between the 2 months is negative, the last month is not a whole month.
+    else if(roundUpFractionalMonths!==true && daysDifference<0)
+    {
+        monthCorrection=-1;
+    }
+
+    return (inverse?-1:1)*(yearsDifference*12+monthsDifference+monthCorrection);
 };
 
 /**
@@ -96,6 +161,18 @@ Utils.calculateDuration = function(startDateTime, endDateTime) {
 };
 
 /**
+ * Calulates age in years
+ * @param birthday {Date} the birthdate. Non-nullable.
+ * @returns {number} age in years
+ * See http://stackoverflow.com/questions/4060004/calculate-age-in-javascript
+ */
+Utils.calculateAge = function(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
+
+/**
  * Formats a string representing the specified duration.
  * @param duration {number} duration in milliseconds to format. Non-nullable.
  * 
@@ -108,16 +185,29 @@ Utils.formatDuration = function(duration) {
     var retval = "";
 
     if( days > 0 ) {
-    	retval += days + " day" + Utils.pluralizeIfNeeded(days) +  ", ";
+    	retval += days + " day" + Utils.pluralizeIfNeeded(days);
+    	if( hours > 0 && minutes > 0 ) {
+    		retval += ", ";
+    	} else if( hours > 0 || minutes > 0 ) {
+    		retval += " and ";
+    	}
     }
     if( hours > 0 ) {
     	retval += hours + " hour" + Utils.pluralizeIfNeeded(hours);
+    	if( days > 0 && minutes > 0) {
+    		//"Who gives a fuck about an oxford comma"
+    		retval += ",";
+    	}
     	if( minutes > 0 ) {
     		retval += " and ";
     	}
     }
     if( minutes > 0 ) {
     	retval += minutes + " minute" + Utils.pluralizeIfNeeded(minutes);
+    }
+    
+    if( days === 0 && hours === 0 && minutes === 0 ) {
+    	retval += "0 minutes";
     }
     
     return retval;
@@ -131,17 +221,27 @@ Utils.formatDuration = function(duration) {
  */
 Utils.getTime = function(dateTime) {
 	var am = true;
-	var hours = dateTime.getHours() - 4; //EST ofsset TODO: Make timezone configurable
-	if( hours > 12 ) {
+	var hours = dateTime.getHours() - 4; //EST offset TODO: Make timezone configurable
+	if( hours >= 12 ) {
 		am = false;
+	}
+	if( hours > 0 && hours > 12 ) {
 		hours = hours - 12;
+	} else if( hours === 0 ) {
+		hours = 12;
 	}
+	
 	var minutes = dateTime.getMinutes();
-	var retval = hours + " ";
-	if( minutes < 10 ) {
-		retval += " oh ";
+	var retval = hours;
+	if( minutes === 0 ) {
+		retval += " oh clock";
 	}
-	retval += minutes;
+	else if( minutes < 10 ) {
+		retval += " oh " + minutes;
+	} else {
+		retval += " " + minutes;
+	}
+	
 	if( am ) {
 		retval += " AM";
 	} else {
